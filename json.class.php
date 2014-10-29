084047e578ea1b335874617e495def1143b3f907
Marcelo Menezes <menezes_07@hotmail.com>
2014-10-29 17:24:28 -0300

    Comentários
    Adicionado comentários.

--- json.class.php
+++ json.class.php
@@ -1,7 +1,16 @@
 <?php
+
+/*
+ * Classe responsável pelo Encode dos dados em jSON.
+ */
+
 class Json{
-	
+	/*
+	 * Função que faz o encode jSon.
+	 *
+	 * Return jSON file.
+	 */
 	public function encode($dados){ 
-		echo json_encode($dados, 128);
+		echo json_encode($dados, 128); //128 = JSON_PRETTY_PRINT
 	}
 }
--- usuarios.class.php
+++ usuarios.class.php
@@ -3,27 +3,40 @@
 require_once 'json.class.php';
 require_once 'conexao.php';
 
+//Cria um instancia da classe Json. 
+$Json = new Json();
+
+
+/*
+ * Classe responsável pela listagem de usuário.
+ */
+
 class Usuarios {
 		
 	public function __construct(){
 
 	}
 	
-	// Usuários
+	
+	/*
+	* Função que exibe todos os usuários. 
+	* 
+	* Return array encoded JSON.
+	*/
+	
 	function getUsuarios(){
 		$resultado = mysql_query("SELECT * FROM usuarios");
 		$num_rows = mysql_num_rows($resultado);
 				
 		//Tratando os erros.
-		
 		//Se retornar erro de tabela.
 		if (!$resultado){
-			$dados['Mensagem'] = 'Não foi possivel selecionar a Tabela.';
+			$dados['Mensagem'] = 'Não foi possivel executar a query';
 		}
 
 		//Se o numero de Registro for menor que 1.
 		else if ($num_rows < 1){
-			$dados['Mensagem'] = 'Não há nenhum dado cadastrado.';
+			$dados['Mensagem'] = 'Nenhum registro encontrado.';
 		}
 		
 		//Se não houver erros na consulta.
@@ -34,42 +47,41 @@ class Usuarios {
 				$dados['dados'][$i] = mysql_fetch_assoc($resultado);
 			}
 			
-			$dados['Mensagem'] = $i . ' registros encontrados.';
+			$dados['Mensagem'] = $num_rows . ' registros encontrados.';
 		}
 
 		return Json::encode($dados);
 	}
 	
 	
+	/*
+	* Função que exibe usuário por nome. 
+	*
+	* Return array encoded JSON.
+	*/
 	function getUsuariosByNome($nome){
-		
 		$query = sprintf("SELECT * FROM usuarios WHERE usuario = '%s'", mysql_real_escape_string($nome));
 		$resultado = mysql_query($query);
 		$num_rows = mysql_num_rows($resultado);
 				
 		//Tratando os erros.
-		
 		//Se retornar erro de tabela.
 		if (!$resultado){
-			$dados['Mensagem'] = 'Não foi possivel selecionar a Tabela.';
+			$dados['Mensagem'] = 'Não foi possivel executar a query.';
 		}
 
 		//Se o numero de Registro for menor que 1.
 		else if ($num_rows < 1){
-			$dados['Mensagem'] = 'Não há nenhum dado cadastrado.';
+			$dados['Mensagem'] = 'Nenhum registro encontrado.';
 		}
 		
 		//Se não houver erros na consulta.
 		else {
-			
 			$dados['dados'] = mysql_fetch_assoc($resultado);
-						
-			$dados['Mensagem'] = 'registros encontrados.';
+			$dados['Mensagem'] = $num_rows 'registro encontrados.';
 		}
 
 		return Json::encode($dados);
 	}
 	
 }
-
-$Json = new Json();
